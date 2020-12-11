89606d378934563fcedcc596b8f827e1ab4b2b30
liuying0125 <1214260258@qq.com>
2020-12-11 14:36:51 +0800

    Stack

---
+++ Queue/array_queue.h
@@ -0,0 +1,20 @@
+/***
+ * File name:array_queue.h
+ * author: liuying
+ * time : 2020-12-11
+ */
+#ifndef ARRAY_QUEUE_H
+#define ARRAY_QUEUE_H
+
+typedef struct _array_queue{
+    int size;
+    int num;
+    int head;
+    int tail;
+    int *array;
+}array_queue;
+
+#define array_queue_is_empty(array_queue) (array_queue->num == 0)
+#define array_queue_is_full(array_queue)  ((array_queue->num) == (array_queue->size))
+
+#endif
